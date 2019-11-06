8eff52df1465c5f9ed8b9d3e8ab5eb22a1bac0d8
WoodySUN <57243680+WoodySun1994@users.noreply.github.com>
2019-11-06 16:00:13 +0800

    Update TracksPlot.py
    按帧显示第3、12、20条航迹，并加入虚假点

--- TracksPlot.py
+++ TracksPlot.py
@@ -1,11 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+更新：“2019.11.06
+#功能：显示选择的航迹并加入虚假点
+#auther： woody sun
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.ion()
 
+#产生噪声点，t为产生的虚假点数
+def FackPointsGene(t):
+    names = ['Speed','X_position','Y_position','Angle','Target']
+    FakePoints = pd.DataFrame(data = None,columns = names)
+    randomspeed = [random.randint(-8,15)for i in range(t)]
+    randomxpos = [round(10.0 * random.uniform(-1,1),3)for i in range(t)]
+    randomypos = [round(40.0 * random.random(),3)for i in range(t)]
+    randomangle = [round(random.uniform(-30,70),1) for i in range(t)]
 
-# 采集航迹点
-plot_en = 1  # 设置是否需要画出航迹
-track_total = 35  # 设置需要采集的航迹数量
+    FakePoints['Speed'] = randomspeed
+    FakePoints['X_position'] = randomxpos
+    FakePoints['Y_position'] = randomypos
+    FakePoints['Angle'] =randomangle
+    FakePoints['Target'] = 0
+
+    return FakePoints    #返回虚假点
+
 
 tracks_no = [3, 12, 20]
 j = 0
@@ -14,7 +40,6 @@ j = 0
 # color_index = 0
 
 for i in tracks_no:
-
     path = 'G:/Graduate/CodeForGuaduate/pysource/tracks/Tracks_' + str(i) + '.txt'
     names = ['Track_No', 'Point_No', 'Speed', 'X_position', 'Y_position', 'Alarm', 'Angle', 'Mat', 'Frame', 'Target']
     try:
@@ -43,8 +68,9 @@ t = max(length1,length2,length3)
 
 x = []
 y = []
+fakex = []
+fakey = []
 for i in range(t):
-
     #读取第i个时间量测的真实航迹
     try:
         x.append(TRACKS.loc['a']['X_position'].iloc[i])
@@ -61,8 +87,20 @@ for i in range(t):
         y.append(TRACKS.loc['c']['Y_position'].iloc[i])
     except:
         pass
-    plt.scatter(x, y, s=5,c = 'b')
+    #产生仿真虚假航迹，每帧噪点数为100
+    FackPoints = FackPointsGene(100)
+    fakex.append(FackPoints['X_position'])
+    fakey.append(FackPoints['Y_position'])
+    plt.ion()
+    plt.scatter(x, y, s=15, c='r')
+    plt.scatter(fakex, fakey, s=5,c = 'gray')
     plt.xlim((-10, 10))
     plt.ylim((0, 30))
     plt.pause(0.001)
+    plt.cla()#清屏
+
+    del x[:]
+    del y[:]
+    del fakex[:]
+    del fakey[:]
 
