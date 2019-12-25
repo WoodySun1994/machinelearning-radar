771aced8026520649787a2f0eb3745ac5eb0e8cc
WoodySUN <57243680+WoodySun1994@users.noreply.github.com>
2019-12-25 17:13:16 +0800

    Update SIMUmain.py

--- SimulationDoc/SIMUmain.py
+++ SimulationDoc/SIMUmain.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+更新：“2019.12.25
+#功能：仿真航迹产生
+#auther： woody sun
+'''
 import SimuGen
 
 def main():
-    SD = SimuGen.SimuData(autotesten = True)
-    SD.create_simu()
-    SD.save_simu()
-    SD.show_simu()
+    for i in range(30):
+        SD = SimuGen.SimuData(autotesten = True)
+        SD.create_simu()
+        SD.save_simu(i)
+        # SD.show_simu()
 
 
 if __name__ == "__main__":
