345d83da809e4cd5084759f25c789b2f23d58980
WoodySUN <57243680+WoodySun1994@users.noreply.github.com>
2019-12-25 17:13:56 +0800

    Update Point.py

--- SimulationDoc/Point.py
+++ SimulationDoc/Point.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-更新：“2019.12.22
+更新：“2019.12.25
 #功能：实现仿真数据点产生
 #auther： woody sun
 '''
@@ -34,43 +34,42 @@ class RealPoint(Point):
         self.color = 'red'
         self.flag = 1
 
+
 class FakePoint(Point):
     def __init__(self, x=0, y=0, speed=0, angle=0, frameno=0):
         super().__init__(x, y, speed, angle, frameno)
-        self.color = 'blue'
+        self.color = 'gray'
         self.flag = 0
-
 
 class RealPtGenerator(RealPoint):
     def __init__(self,x,y,movetype,speed,accelerate=0,angle =0):
         self.movetype = movetype
         if self.movetype == 'UL':#匀速直线运动
             self.accelerate = 0
-            self.speed = self.speed = round(speed + self.accelerate, 2)
-            self.y = round(y - self.speed/36, 2)# 将km/s改为m/s
             self.angle = round(angle, 3)
-            self.x = round(x - speed / 36 * math.tan(math.radians(self.angle)), 2)
+
         elif self.movetype == 'AL':#加速直线运动
             self.accelerate = round(accelerate, 2)
-            self.speed = round(speed + self.accelerate, 2)
-            self.y = round(y - self.speed/36, 2)
             self.angle = round(angle, 3)
-            self.x = round(x - speed/36 * math.tan(math.radians(self.angle)), 2)
+
         elif self.movetype == 'UT': #匀速转弯运动
             self.angle = round(angle + (angle/4), 3)
-            if self.angle > 70:  #转弯角度不超过正负70°
-                self.angle = 70
+            if self.angle > 90:  #转弯角度不超过正负70°
+                self.angle = 90
 
-            elif self.angle < -70:
-                self.angle = -70
+            elif self.angle < -90:
+                self.angle = -90
             self.accelerate = 0
-            self.speed = round(speed + self.accelerate, 2)
-            self.y = round(y - self.speed/36, 2)
 
-            self.x = round(x - speed/36 * math.tan(math.radians(self.angle)), 2)
+        self.speed = round(speed + self.accelerate, 2)
+        speedy = self.speed * math.cos(math.radians(self.angle))  # y方向上的速度分量
+        speedx = self.speed * math.sin(math.radians(self.angle))  # x方向上的速度分量
+        self.y = round(y - self.speed / 36 * speedy, 2)  # 将km/s改为m/s
+        self.x = round(x - speed / 36 * speedx, 2)
 
         self.color = 'red'
         self.flag = 1
+        self.size = 5
 
     def display(self):
         print("(x,y): (",self.x,",",self.y,")" , "   speed:" , self.speed , "  angle:" ,self.angle,'frameno:')
@@ -82,8 +81,9 @@ class FakePtGenerator(FakePoint):
         self.y = round(40 * random.uniform(0, 1),2)
         self.x = round(30 * random.uniform(-1,1),2)
         self.angle = round(math.degrees(math.atan(self.x / (self.y + 0.01))), 3)
-        self.color= 'blue'
+        self.color= 'gray'
         self.flag = 0
+        self.size = 2
 
     def display(self):
         print("(x,y): (", self.x, ",", self.y, ")", "   speed:", self.speed, "  angle:", self.angle)
