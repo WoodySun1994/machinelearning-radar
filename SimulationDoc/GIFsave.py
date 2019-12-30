d89800519b5d10af3ccfcd30635b99d5eac1d45d
WoodySUN <57243680+WoodySun1994@users.noreply.github.com>
2019-12-30 19:01:06 +0800

    Update GIFsave.py

--- SimulationDoc/GIFsave.py
+++ SimulationDoc/GIFsave.py
@@ -3,7 +3,7 @@
 
 '''
 更新：“2019.12.24
-#功能：绘制航迹并保存为GIF，输入为txt文件。内容顺序格式为：[ 'Speed', 'Angle',  'Target','X_position', 'Y_position']
+#功能：绘制航迹并保存为GIF，输入为txt文件。内容顺序格式为：[ 'Speed', 'Angle','X_position', 'Y_position',  'Target']
 #auther： woody sun
 '''
 import matplotlib.pyplot as plt
@@ -11,38 +11,38 @@ import matplotlib.animation as animation
 import pandas as pd
 
 fig,ax = plt.subplots()
-xdata, ydata = [], []
-tln,  = plt.plot([],[],'o',color = 'red', animated = True)
-fln, = plt.plot([],[],'o',color = 'gray', markersize = 2,animated = True)
+xData, yData = [], []
+trueLine,  = plt.plot([],[],'o',color = 'red', animated = True)
+fakeLine, = plt.plot([],[],'o',color = 'gray', markersize = 2,animated = True)
 
-def init():
+def Init():
     '''动画对象初始化函数'''
     ax.set_xlim(-30,30)
     ax.set_ylim(0,40)
-    return tln,fln,
+    return trueLine,fakeLine,
 
 
-def update(frames):
+def Update(frames):
     '''动画对象更新函数'''
-    framepath = './simufile/simufile_' + str(frames) + '.txt'
-    names = [ 'Speed', 'Angle',  'Target','X_position', 'Y_position']
-    tracks = pd.read_csv(framepath, sep=' ', names=names)
-    faketracks = tracks[tracks['Target'].isin([0])]  # 选择当前航迹中所有虚假点
-    xdata = faketracks['X_position']
-    ydata = faketracks['Y_position']
-    fln.set_data(xdata, ydata)
-    truetracks = tracks[tracks['Target'].isin([1])]  # 选择当前航迹中所有真实点
-    xdata = truetracks['X_position']
-    ydata = truetracks['Y_position']
-    tln.set_data(xdata,ydata)
+    framePath = './simufile0/frame_' + str(frames) + '.txt'
+    names = [ 'Speed', 'Angle','X_position', 'Y_position',  'Target']
+    tracks = pd.read_csv(framePath, sep=' ', names=names)
+    fakeTracks = tracks[tracks['Target'].isin([0])]  # 选择当前航迹中所有虚假点
+    xData = fakeTracks['X_position']
+    yData = fakeTracks['Y_position']
+    fakeLine.set_data(xData, yData)
+    trueTracks = tracks[tracks['Target'].isin([1])]  # 选择当前航迹中所有真实点
+    xData = trueTracks['X_position']
+    yData = trueTracks['Y_position']
+    trueLine.set_data(xData,yData)
 
-    return tln,fln,
+    return trueLine,fakeLine,
 
 
 def main():
-    anim = animation.FuncAnimation(fig, update, frames=range(10), interval=400, init_func=init,blit=True)
+    anim = animation.FuncAnimation(fig, Update, frames=range(10), interval=400, init_func=Init,blit=True)
     plt.show()
-    anim.save('test_animation.gif', writer='imagemagick')
+    anim.save('simu0.gif', writer='imagemagick')
 
 if __name__ == '__main__':
     main()
